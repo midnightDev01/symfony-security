0f2da158c69a35fbc2b68ef5b3b343e6e149d7d9
zerotheone01 <raissa.developer@gmail.com>
2022-07-13 21:04:31 +0200

    Delete projects/docker-ticket-center directory

--- projects/docker-ticket-center/add_host.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. ./.env
-
-exists=$(cat "/etc/hosts" | grep "${APP_NAME}")
-if [ -z "${exists}" ]; then
-  echo "127.0.0.1 ${APP_NAME}.docker" | sudo tee -a /etc/hosts >/dev/null
-fi
--- projects/docker-ticket-center/etc/init-mongo-js.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mongosh <<EOF
-db = connect("localhost:27017","admin","changeme");
-use myra-app;
-db.createCollection("cars")
-EOF
